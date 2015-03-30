0f95912d791ff93ba15799617b3329d187472bcd
piwatake <piwatake2011@knights.ucf.edu>
2015-03-30 23:34:15 +0000

    -

--- html/js/site.js
+++ html/js/site.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
     var tagline = document.querySelector("p.tagline");
-    tagline.innerText = "From Cloud9 IDE!";
+    tagline.innerText = "From the cloud!";
 });
